c5749cfa7f93469497f4311f45f8c53e017adad1
ai96yuz <ai96yuz@gmail.com>
2021-09-18 19:19:23 +0300

    Implement order template, with list of active orders. Added method delete order

--- order/urls.py
+++ order/urls.py
@@ -1,12 +1,10 @@
-# from book.views import book_detail_view
 from django.urls import path
-from .views import AllOrdersCreated, book_by_user, home, AllOrdersPlated, HandOverBook
 from . import views
 
-urlpatterns = [
-    path("", views.home, name='orders'),
-    path("created/", AllOrdersCreated.as_view(), name='orders_created'),
-    path("plated/", AllOrdersPlated.as_view(), name='orders_plated'),
-    path("handover/",  HandOverBook.as_view(), name='hand_over_books'),
-    path("user/111/", book_by_user, name='book_by_userId')
-]
+urlpatterns = \
+    [
+        path('', views.orders, name='orders'),
+        path('delete/<int:order_id>/', views.delete_order),
+        path('form', views.orders_form, name='create_orders'),
+        path('form/<int:order_id>/', views.orders_form, name='update_orders'),
+    ]
