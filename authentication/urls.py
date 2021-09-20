ed980e2c1c439360dd0132c1aae069fafae2684f
ai96yuz <ai96yuz@gmail.com>
2021-09-20 23:58:51 +0300

    Implemented django rest. Created API: api/v1/user, api/v1/user/<id>, api/v1/user/<id>/order. Created serializer.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from .views import *
 
 urlpatterns = \
     [
@@ -8,4 +9,7 @@ urlpatterns = \
         path('delete/<int:user_id>/', views.delete_user),
         path('signup/', views.register, name="create_user"),
         path('signup/<int:user_id>/', views.update_user, name="update_user"),
+        path('api/v1/user/', UserListCreate.as_view()),
+        path('api/v1/user/<int:pk>/', UserViewUpdateDelete.as_view()),
+        path('api/v1/user/<int:user_pk>/order/', OrdersListForUser.as_view()),
     ]
